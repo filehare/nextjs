5792f0c99e2d3fa38ec0731cca41e9cb33998994
filehare <88797046+filehare@users.noreply.github.com>
2021-10-12 00:52:33 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://filehare.com/">FileHare</a>
+          Welcome to <a href="https://filehare.com/">FileHare</a> Project
         </h1>
 
         <p className={styles.description}>
